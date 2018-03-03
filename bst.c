219987e024e7d75995d854df9bc5255bc3f2ca7c
Kevin Joseph Becker <kjb2503@cs.rit.edu>
2018-03-02 19:10:13 -0500

    Built the tree creation. (build_tree) Added in two static "helper" functions for the creation of, and insertion of new nodes.

--- bst.c
+++ bst.c
@@ -10,9 +10,58 @@
 // } TreeNode;
 
 
+static TreeNode *newTreeNode(int d)
+{
+    // creates a new node
+    TreeNode *node = NULL;
+
+    // allocates enough space for node
+    node = malloc(sizeof(TreeNode));
+
+    // sets our data to d
+    node->data = d;
+    // sets our left and right to NULL, we don't know them yet
+    node->left = NULL;
+    node->right = NULL;
+
+    // returns our newly created node
+    return node;
+}
+
+static void insertChildNode(TreeNode *root, TreeNode *child)
+{
+    // checks if we need to go left (child data <= data goes left)
+    if(child->data <= root->data)
+    {
+        // checks if the left node is a leaf or not
+        if(root->left != NULL)
+            insertChildNode(root->left, child);
+        else
+            root->left = child;
+    }
+    // if we don't need to go left, the only place we can go is right
+    else
+    {
+        // checks if the right node is a leaf or not
+        if(root->right != NULL)
+            insertChildNode(root->right, child);
+        else
+            root->right = child;
+    }
+}
+
+
 void build_tree(TreeNode** root, const int elements[], const int count)
 {
-    // builds our tree
+    int i = 0;
+    // our first element in hardcoded (we're guaranteed this many nodes)
+    TreeNode *start = newTreeNode(elements[i++]);
+
+    //goes through each element and inserts it
+    for(; i < count; ++i)
+        insertChildNode(start, newTreeNode(elements[i]));
+
+    root = &start;
 }
 
 
@@ -71,6 +120,13 @@ int main(int argc, char **argv)
     // prints our numbers we read in
     for(int i = 0; i < numNodes; ++i)
         printf("%d\n", nums[i]);
+
+    // BEGIN TREE BUILDING PROCEDURE ===========================================
+
+    // creates a root node
+    TreeNode *root;
+    // builds our tree using root
+    build_tree(&root, nums, numNodes);
 
     return EXIT_SUCCESS;
 }
